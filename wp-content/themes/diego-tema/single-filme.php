89218c3ba45740ca2ee170af36928c2388b245cb
DiegoFeitoza <diegofeitoza@yahoo.com.br>
2019-01-17 16:06:46 -0200

    Atualizando bibliotecas

--- wp-content/themes/diego-tema/single-filme.php
+++ wp-content/themes/diego-tema/single-filme.php
@@ -4,54 +4,59 @@
 	<!-- section -->
 	<section>
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
+	<div class="container">
+	<div class="row">
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
-
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
-
-			<!-- post details -->
-			<span class="date">Adicionado em: <?php the_time('d \d\e F \d\e Y'); ?></span> | 
-			<span class="author"> por: <?php the_author_posts_link(); ?></span>			
-			<!-- /post details -->
-
-			<!-- detalhes do filme -->
-			<p>
-				Diretor: <?php echo get_field('diretor') ?> | 
-				Duração: <?php echo get_field('duracao') ?> | 
-				<?php echo get_the_term_list( $post->ID, 'categoria-filmes', 'Categorias: ', ', ') ?> | 
-				Classificação: 
-			 	<?php
-					$tags= get_the_terms( $id, 'idade');
-					foreach($tags as $tag): 
-				?>
-					<a class="link-classificacao" href="<?php echo get_tag_link( $tag->term_id ) ?>" target="_self">
-						<?php echo $tag->name ?>
+			<div class="col-sm-4">
+				<!-- post thumbnail -->
+				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+						<?php the_post_thumbnail(); // Fullsize image for the single post ?>
 					</a>
-				<?php endforeach;?>	
-			<p>			
-			<h3>Sinopse:</h3> 
-			<?php echo the_content(); // Dynamic Content ?>		
+				<?php endif; ?>
+				<!-- /post thumbnail -->
+			</div>
 
-			<button>
-				<?php edit_post_link('Editar conteudo'); // Always handy to have Edit Post Links available ?>
-			</button>
+			<div class="col-sm-8">
+				<!-- post title -->
+				<h1>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h1>
+				<!-- /post title -->
 
+				<!-- post details -->
+				<span class="date">Adicionado em: <?php the_time('d \d\e F \d\e Y'); ?></span> | 
+				<span class="author"> por: <?php the_author_posts_link(); ?></span>			
+				<!-- /post details -->
+
+				<!-- detalhes do filme -->
+				<p>
+					Diretor: <?php echo get_field('diretor') ?> | 
+					Duração: <?php echo get_field('duracao') ?> | 
+					<?php echo get_the_term_list( $post->ID, 'categoria-filmes', 'Categorias: ', ', ') ?> | 
+					Classificação: 
+					<?php
+						$tags= get_the_terms( $id, 'idade');
+						foreach($tags as $tag): 
+					?>
+						<a class="link-classificacao" href="<?php echo get_tag_link( $tag->term_id ) ?>" target="_self">
+							<?php echo $tag->name ?>
+						</a>
+					<?php endforeach;?>	
+				<p>			
+				<h3>Sinopse:</h3> 
+				<?php echo the_content(); // Dynamic Content ?>		
+
+				<button>
+					<?php edit_post_link('Editar conteudo'); // Always handy to have Edit Post Links available ?>
+				</button>
+			</div>
 		</article>
 		<!-- /article -->
-
+	</div>
+	</div>
 	<?php endwhile; ?>
 
 	<?php else: ?>
