ef4bf6d020fb463a6b289735fac026d7cb56015a
Feitoza <diego.feitoza@accenture.com>
2019-02-11 19:05:34 -0200

    Atualizando arquivos

--- wp-content/themes/diego-tema/single-filme.php
+++ wp-content/themes/diego-tema/single-filme.php
@@ -2,89 +2,87 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="col-xl-10">
-				<main role="main">
-					<!-- section -->
-					<section>
-					<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-					<div class="container">
-					<div class="row">
-						<!-- article -->
-						<article id="post-<?php the_ID(); ?>" <?php post_class('article-single-filme'); ?>>
+			<main role="main">
+				<!-- section -->
+				<section>
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				<div class="container">
+				<div class="row">
+					<!-- article -->
+					<article id="post-<?php the_ID(); ?>" <?php post_class('article-single-filme'); ?>>
+						<div class="row">
+						<div class="col-12">
+							<!-- post thumbnail -->
+							<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+									<?php the_post_thumbnail(); // Fullsize image for the single post ?>
+								</a>
+							<?php endif; ?>
+							<!-- /post thumbnail -->
+						</div>
+						</div>
+						<div class="col-12">
+							<!-- post title -->
+							<h1>
+								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+							</h1>
+							<!-- /post title -->
 
-							<div class="col-xl-4">
-								<!-- post thumbnail -->
-								<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-									<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-										<?php the_post_thumbnail(); // Fullsize image for the single post ?>
+							<!-- post details -->
+							<span class="date">Adicionado em: <?php the_time('d \d\e F \d\e Y'); ?></span> | 
+							<span class="author"> por: <?php the_author_posts_link(); ?></span>			
+							<!-- /post details -->
+
+							<!-- detalhes do filme -->
+							<p>
+								Diretor: <?php echo get_field('diretor') ?> | 
+								Duração: <?php echo get_field('duracao') ?> | 
+								<?php echo get_the_term_list( $post->ID, 'categoria-filmes', 'Categorias: ', ', ') ?> | 
+								Classificação: 
+								<?php
+									$tags= get_the_terms( $id, 'idade');
+									foreach($tags as $tag): 
+								?>
+									<a class="link-classificacao" href="<?php echo get_tag_link( $tag->term_id ) ?>" target="_self">
+										<?php echo $tag->name ?>
 									</a>
-								<?php endif; ?>
-								<!-- /post thumbnail -->
-							</div>
+								<?php endforeach;?>	
+							<p>			
+							<h3>Sinopse:</h3> 
+							<?php echo the_content(); // Dynamic Content ?>		
 
-							<div class="col-xl-8">
-								<!-- post title -->
-								<h1>
-									<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-								</h1>
-								<!-- /post title -->
+							<?php
+								if(is_user_logged_in()){
+							?>
+								<button>
+									<?php edit_post_link('Editar conteudo'); // Always handy to have Edit Post Links available ?>
+								</button>
+							<?php
+								}
+							?>
+						</div>
+					</article>
+					<!-- /article -->
+				</div>
+				</div>
+				<?php endwhile; ?>
 
-								<!-- post details -->
-								<span class="date">Adicionado em: <?php the_time('d \d\e F \d\e Y'); ?></span> | 
-								<span class="author"> por: <?php the_author_posts_link(); ?></span>			
-								<!-- /post details -->
+				<?php else: ?>
 
-								<!-- detalhes do filme -->
-								<p>
-									Diretor: <?php echo get_field('diretor') ?> | 
-									Duração: <?php echo get_field('duracao') ?> | 
-									<?php echo get_the_term_list( $post->ID, 'categoria-filmes', 'Categorias: ', ', ') ?> | 
-									Classificação: 
-									<?php
-										$tags= get_the_terms( $id, 'idade');
-										foreach($tags as $tag): 
-									?>
-										<a class="link-classificacao" href="<?php echo get_tag_link( $tag->term_id ) ?>" target="_self">
-											<?php echo $tag->name ?>
-										</a>
-									<?php endforeach;?>	
-								<p>			
-								<h3>Sinopse:</h3> 
-								<?php echo the_content(); // Dynamic Content ?>		
+					<!-- article -->
+					<article>
 
-								<?php
-									if(is_user_logged_in()){
-								?>
-									<button>
-										<?php edit_post_link('Editar conteudo'); // Always handy to have Edit Post Links available ?>
-									</button>
-								<?php
-									}
-								?>
-							</div>
-						</article>
-						<!-- /article -->
-					</div>
-					</div>
-					<?php endwhile; ?>
+						<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
 
-					<?php else: ?>
+					</article>
+					<!-- /article -->
 
-						<!-- article -->
-						<article>
+				<?php endif; ?>
 
-							<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-						</article>
-						<!-- /article -->
-
-					<?php endif; ?>
-
-					</section>
-					<!-- /section -->
-				</main>
-			</div>
-			<div class="col-xl-2">
+				</section>
+				<!-- /section -->
+			</main>
+			<div class="col-xl-2 col-12">
 				<?php get_sidebar(); ?>
 			</div>
 		</div>
